6b3559c6e6708bcb0a47fbb04941ea6330345cec
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-03-26 23:56:32 +0900

    Create 17219.py

---
+++ Backjoon/17219.py
@@ -0,0 +1,12 @@
+N, M = map(int, input().split())
+site = dict()
+pwd = []
+answer = []
+for _ in range(N) :
+  a, b =input().split()
+  site[a] = b
+for _ in range(M) :
+  pwd.append(input())
+
+for i in pwd :
+  print(site[i])
