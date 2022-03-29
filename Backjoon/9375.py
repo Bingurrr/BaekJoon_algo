e4824202cf2703b18067f678fafe7a400062e563
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-03-29 13:28:32 +0900

    Create 9375.py

---
+++ Backjoon/9375.py
@@ -0,0 +1,14 @@
+T = int(input())
+for _ in range(T) :
+  N = int(input()) 
+  clothes = dict()
+  for i in range(N) :
+    a,b = input().split()
+    if clothes.get(b) == None :
+      clothes[b] = set()
+    clothes[b].add(a)
+  count = 1
+
+  for k in clothes.keys():
+    count *= len(clothes[k]) + 1
+  print(count-1)
