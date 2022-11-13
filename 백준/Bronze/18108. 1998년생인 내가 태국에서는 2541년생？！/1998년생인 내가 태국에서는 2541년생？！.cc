7a8c6dd2bbd52a1492c88c3670fd295b78020df8
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-11-13 19:54:53 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    cout << n - 2541 + 1998;
+    
+}
