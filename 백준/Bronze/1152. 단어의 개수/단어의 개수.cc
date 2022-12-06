7320eabe8fa24409484f4e4304465d5cfa52a29e
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-12-06 23:55:53 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+string s;
+int ans;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    while(cin >> s){
+        ans++;
+    }
+    cout << ans;
+}
