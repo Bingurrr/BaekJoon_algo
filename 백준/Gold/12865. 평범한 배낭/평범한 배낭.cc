ac2dffcc3f3d32c6a256f62676d8746ba7fbe0a4
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-12-14 18:20:14 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41476 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,34 +1,30 @@
 #include <iostream>
-#include <algorithm>
-#define MAX_SIZE 101
-#define MAX_INPUT 100001
 
 using namespace std;
 
-int value[MAX_SIZE];
-int weight[MAX_SIZE];
-// int dp[MAX_INPUT];
-int dp[MAX_SIZE][MAX_INPUT];
 int n, k;
+int weight[101];
+int value[101];
+int dp[101][100001];
 
-// 1차 풀이
-// 중복 제거를 못했음 ->dp +  knapsack 알고리즘 적용해야함
 
 int main() {
-	cin >> n >> k;
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++){
+        cin >> weight[i] >> value[i];
+    }
 
-	for (int i = 1; i <= n; i++) {
-		cin >> weight[i] >> value[i];
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = weight[i]; j <= k; j++) {
-			dp[i][j] = max(dp[i-1][j], dp[i-1][j - weight[i]] + value[i]);
-			//dp[j] = max(dp[j], dp[j-weight[i] + value[i]]);
-		}
-		for (int j = 1; j < weight[i]; j++) {
-			dp[i][j] = dp[i - 1][j];
-		}
-	}
-	//for (int i = 0; i <= k; i++) cout << dp[i] << " ";
-	cout << dp[n][k];
+    for(int i = 1; i <= n; i++){
+        for(int j = weight[i]; j <= k; j++){
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]);
+        }
+        for(int j = 1; j < weight[i]; j++){
+            dp[i][j] = dp[i-1][j];
+        }
+    }
+    cout << dp[n][k];
+	return 0;
 }
