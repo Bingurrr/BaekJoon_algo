d3b43665c623f881774bd72e2a4d3cf30188f996
박병규 <39543006+Bingurrr@users.noreply.github.com>
2022-12-31 04:26:34 +0900

    [Silver II] Title: 좌표 압축, Time: 436 ms, Memory: 18344 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <bitset>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int n, k;
+int last_value, cnt;
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+int ans[1000001];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);	
+	cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> k;
+        pq.push({k,i});
+    }
+    while(!pq.empty()){
+        int t = pq.top().first;
+        int s = pq.top().second;
+        pq.pop();
+        if(t==last_value){
+            ans[s] = cnt-1;
+            continue;
+        }
+        else{
+            last_value = t;
+            ans[s] = cnt;
+            cnt++;
+        }
+    }
+    for(int i = 0; i < n; i++){
+        cout << ans[i] << " ";
+    }
+
+	return 0; 
+}
