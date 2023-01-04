8e38945c50746dff104f6e3509ecf2ecdc2b689a
박병규 <39543006+Bingurrr@users.noreply.github.com>
2023-01-04 21:23:16 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <bitset>
+#include <set>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int n;
+char arr[65][65];
+char ans;
+
+void solve(int x, int y, int gap){
+    char s = arr[y][x];
+    for(int i = y; i < y+gap; i++){
+        for(int j = x; j < x+gap; j++){
+            if(arr[i][j] != s){
+                cout<< '(';
+                solve(x,y, gap/2);
+                solve(x+gap/2,y, gap/2);
+                solve(x,y+gap/2, gap/2);
+                solve(x+gap/2,y+gap/2, gap/2);
+                cout << ')';
+                return;
+            }
+        }
+    }
+    cout << s;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    solve(0,0,n);
+    
+	return 0; 
+}
