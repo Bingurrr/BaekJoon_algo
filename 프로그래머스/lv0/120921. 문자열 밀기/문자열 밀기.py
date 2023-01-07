9ad4fc0749d34c605229c3de9dd1207174a2eb9c
박병규 <39543006+Bingurrr@users.noreply.github.com>
2023-01-08 00:38:54 +0900

    [level 0] Title: 문자열 밀기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.py
@@ -0,0 +1,9 @@
+def solution(A, B):
+    answer = 0
+    if A == B : return 0
+    for i in range(len(A)) :
+        A = A[-1] + A[:-1]
+        answer += 1
+        if A==B : break
+    if A!= B : answer = -1
+    return answer
