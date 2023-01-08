616a5278403c5c3be1a5c8a4a7f00d4783ce840d
박병규 <39543006+Bingurrr@users.noreply.github.com>
2023-01-09 07:06:39 +0900

    [Gold IV] Title: 타일 채우기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.cc
+++ 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.cc
@@ -13,7 +13,7 @@ using namespace std;
 
 int n;
 long long int dp[INF];
-long long int s[INF];
+long long int k;
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -23,14 +23,14 @@ int main(){
     dp[1] = 2;
     dp[2] = 7;
     dp[3] = 22;
-    s[2] = 1;
-    s[3] = 1;
+
+    k = 1;
     long long int a = 2;
     long long int b = 3;
     long long int c = 2;
     for(int i = 4; i <= n; i++){
-        s[i] = (dp[i-3] + s[i-1]);
-        dp[i] = (s[i]*a + dp[i-2]*b + dp[i-1]*c) %mod;
+        k = (dp[i-3] +k);
+        dp[i] = (k*a + dp[i-2]*b + dp[i-1]*c) %mod;
     }
     cout << dp[n];
 	return 0; 
