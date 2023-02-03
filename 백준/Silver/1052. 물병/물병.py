3eca6d47e1b34d59e04c608dfaa9f1b97794bbd6
박병규 <39543006+Bingurrr@users.noreply.github.com>
2023-02-04 02:57:31 +0900

    [Silver I] Title: 물병, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1052. 물병/물병.py
+++ 백준/Silver/1052. 물병/물병.py
@@ -1,40 +1,25 @@
 import math
+import sys
 # 입력
 N, K = map(int,input().split())
 
-""" 
- N개의 물병을 가지고 있음
- 처음에는 N개의 물통에 1L씩 물이 들어있음
- 다른 장소로 물병을 옮기고 싶음 -> 한번옮길 수 있고 K개 옮길 수 있음 따라서 물병들을 합쳐야함
-
- K개가 넘지 않은 비어있는 물병을 만들고 싶음
- -> 같은 양의 물이 들어있는 물병을 두개 고르고 이를 합칠 수 있다
-
-  내 생각
-  2**x1 + 2**x2 + ... + 2**xk <= N+answer -> answer가 최솟이 되게해야함 N, K 는 주어짐
-  따라서 2의 제곱의 합들이 최소가 되야햠
-  answer = 2의 k승들의 합 - N
-
-  2**x(k비트 숫자)
-"""
-
+# 물병을 구입할 필요가 없을 때
 if N <= K :
   print(0)
 else :
-  a = N
-  sum = 0
-  tmp = 0
+  input_N = N
+  sum = 0 # 2의 거듭제곱의 합으로 나타내줘야함
+  tmp = 0 # 남은 N값으로 만들 수 있는 2의 거듭제곱중 가장 큰 값
   while K != 0 :
     if N == 0 : 
       break
     tmp = 2**(int(math.log(N,2)))
-    if N == 1 : tmp = 1
     if K == 1 :
       tmp = 2**(int(math.log(N,2))+1)
       if math.log(N,2) == int(math.log(N,2)) :
         tmp = int(2**(math.log(N,2)))
     N = N - tmp
     K = K - 1
-    sum = sum + tmp
+    sum = sum + tmp 
     
-  print(sum - a)
+  print(sum - input_N)
