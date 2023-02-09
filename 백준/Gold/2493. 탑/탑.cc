31346dc2933969a0f0276a478e8a845a543b2de0
박병규 <39543006+Bingurrr@users.noreply.github.com>
2023-02-10 00:15:43 +0900

    [Gold V] Title: 탑, Time: 100 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -12,7 +12,6 @@
 using namespace std;
 
 int n, k;
-// int arr[500001];
 stack<pair<int,int>> st;
 
 
@@ -21,17 +20,18 @@ int main(){
     cin.tie(0);
     cout.tie(0);
     cin >> n;
+    
     for(int i = 1; i <= n; i++){
         cin >> k;
 
         while(!st.empty()){
             if(st.top().second > k){
-                cout << st.top().first << ' ';
                 break;
             }
             st.pop();
         }
-        if(st.empty()) cout << 0 << ' ';
+
+        cout << (st.empty()?0:st.top().first) << ' ';
 
         st.push({i,k});
     }
