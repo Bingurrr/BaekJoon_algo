e5662b6718afb19fb8b5774e64e40e65e6f62e5f
박병규 <39543006+Bingurrr@users.noreply.github.com>
2023-02-17 00:12:28 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i=0; i<n; i++) {
+        cin >> a[i];
+    }
+    sort(a.rbegin(), a.rend());
+    cout << a[k-1] << endl;
+    return 0;
+}
