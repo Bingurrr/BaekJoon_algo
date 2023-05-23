bdfc3bbb37358ac6a87bc24b009e1bc532da4965
박병규 <pbk5485@gmail.com>
2023-05-24 02:31:57 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int a, b;
+int main(void){
+    
+    cin >> a >> b;
+    
+    if(b < 45){
+        b += 15;
+        a--;
+    }
+    else
+        b -= 45;
+    
+    if(a < 0)
+        a = 23;
+    
+    cout << a << " "<< b;
+}
