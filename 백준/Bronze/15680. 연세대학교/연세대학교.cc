5b2213b81b11f75899479eef4481162b20464011
박병규 <pbk5485@gmail.com>
2023-07-16 22:36:25 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <bitset>
+#include <set>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <stack>
+#include <regex>
+#define INF (1 << 30)
+
+using namespace std;
+
+int n;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    if(n==0){
+        cout << "YONSEI";
+    }
+    else {
+        cout << "Leading the Way to the Future";
+    }
+   
+    
+    return 0;
+}
+
+
