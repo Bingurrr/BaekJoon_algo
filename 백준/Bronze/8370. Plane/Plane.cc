8adb6d40897e5d6f0f21fdc37a0e58fa043b871f
박병규 <pbk5485@gmail.com>
2023-09-01 02:01:01 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <bitset>
+#include <set>
+#include <vector>
+#include <cstring>
+#include <set>
+#include <stack>
+#include <regex>
+#define INF (1 << 30)
+
+using namespace std;
+
+int a, b, c, d;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> a >> b >> c >> d;
+    cout << a * b + c * d;
+
+    return 0;
+}
+
+
