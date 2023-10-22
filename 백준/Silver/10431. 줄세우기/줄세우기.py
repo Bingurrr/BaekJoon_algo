5dc0fc5dbee37adbd3d27e521980d2d8708cc57e
박병규 <pbk5485@gmail.com>
2023-10-22 20:51:59 +0900

    [Silver V] Title: 줄세우기, Time: 160 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.py
+++ 백준/Silver/10431. 줄세우기/줄세우기.py
@@ -9,11 +9,12 @@ def swap(a,b,arr) :
 
 def sol(arr) :
     cnt = 0
-    for i in range(len(arr)-2, -1, -1) :
-        if arr[i] > arr[i+1] :
-            arr = swap(i, i+1, arr)
-            cnt += 1
-    return cnt, arr
+    for j in range(len(arr)) :
+        for i in range(j-1, -1, -1) :
+            if arr[i] > arr[i+1] :
+                arr = swap(i, i+1, arr)
+                cnt += 1
+    return cnt
 
 
 while p > 0 :
@@ -23,11 +24,7 @@ while p > 0 :
     arr = list(map(int, input().split()))
     testcase = arr[0]
     students = arr[1:]
-    line = []
     
-    for i in range(len(students)) :
-        line.append(students[i])
-        cnt, line = sol(line)
-        ans += cnt
+    ans = sol(students)
 
     print(testcase, ans)
